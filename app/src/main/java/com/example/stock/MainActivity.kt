777fe61dae59600d722f526dd8b25b2092131620
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-07 15:48:18 +0900

    Add kakao login

--- app/src/main/java/com/example/stock/MainActivity.kt
+++ app/src/main/java/com/example/stock/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.stock
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
@@ -8,8 +9,16 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import com.example.stock.ui.main.SectionsPagerAdapter
 import com.example.stock.databinding.ActivityMainBinding
+import com.kakao.sdk.auth.LoginClient
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.common.model.AuthErrorCause.* //when문에 있는 AccessDenied와 같은 상수들을 사용하기 위함
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +39,10 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
+        val keyHash = Utility.getKeyHash(this)  //해쉬 키 값을 받기 위함
+        Log.d("Hash", keyHash)
+
     }
 }
