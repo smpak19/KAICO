ebd4ee95afd4cfd282db652347e66478b60d7c53
Jinnam <smpak@kaist.ac.kr>
2022-01-10 17:14:25 +0900

    tab2 adapter

--- app/src/main/java/com/example/stock/CoinInfo.kt
+++ app/src/main/java/com/example/stock/CoinInfo.kt
@@ -5,3 +5,7 @@ data class CoinInfo (val name : String,
                      val price : String,
                      val rate : String,
                      val total : String)
+
+// coinname, ticker, gain, yield, amount, avg, total, buy
+data class CoinListInfo(val coinname: String, val ticker: String, val gain: String, val yield: String,
+                        val amount: String, val avg: String, val total: String, val buy: String)
