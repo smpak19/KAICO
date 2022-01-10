cbf5ebce412dff15a713cf356692fa89af0cbb9d
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-10 17:19:11 +0900

    Merge branch 'master' of https://github.com/smpak19/Stock into layout
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/stock/Fragment3.kt
+++ app/src/main/java/com/example/stock/Fragment3.kt
@@ -5,10 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.stock.databinding.FragmentBlankBinding
+import com.example.stock.databinding.FragmentTab3Binding
+
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.*
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import io.socket.emitter.Emitter
+import org.json.JSONArray
+import org.json.JSONException
+import java.text.DecimalFormat
 
 class Fragment3 : Fragment() {
-    private var _binding: FragmentBlankBinding? = null
+    private var _binding: FragmentTab3Binding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -16,7 +31,19 @@ class Fragment3 : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentBlankBinding.inflate(inflater, container, false)
+        _binding = FragmentTab3Binding.inflate(inflater, container, false)
+
+        GlobalApplication.mSocket.emit("get_account", GlobalApplication.user_id)
+        GlobalApplication.mSocket.on("give_account", Emitter.Listener {
+            val account = toDoubleFormat(JSONArray(it).getDouble(0))
+            binding.username.text = account
+        })
+
+        GlobalApplication.mSocket.emit("get_total", GlobalApplication.user_id)
+        GlobalApplication.mSocket.on("give_total", Emitter.Listener {
+            val total = toDoubleFormat(JSONArray(it).getDouble(0))
+            binding.totalkrw.text = total
+        })
         return binding.root
     }
 
@@ -25,7 +52,27 @@ class Fragment3 : Fragment() {
         _binding = null
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.textview.text = "Hello world!"
+
+
+    fun toDoubleFormat(num: Double): String? {
+        var df: DecimalFormat? = null
+        df = when {
+            num in 100.0..999.9 -> {
+                DecimalFormat("000.0")
+            }
+            num in 10.0..99.99 -> {
+                DecimalFormat("00.00")
+            }
+            num in 1.0..9.9999 -> {
+                DecimalFormat("0.000")
+            }
+            num < 1 -> {
+                DecimalFormat("0.0000")
+            }
+            else -> {
+                DecimalFormat("###,###,###")
+            }
+        }
+        return df.format(num)
     }
 }
